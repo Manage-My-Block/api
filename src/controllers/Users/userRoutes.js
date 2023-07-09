cbb606889cd400442eb43fe500beca948d997aab
socialtonics <mjkgarrow@gmail.com>
2023-07-09 11:36:24 +1000

    improved auth and user validation on user routes

--- src/controllers/Users/userRoutes.js
+++ src/controllers/Users/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateUser } = require('../../middleware/userValidation');
+const { validateNewUser, validateUpdateUser, validateDeleteUser } = require('../../middleware/userValidation');
 const { authenticateUser } = require('../../middleware/authentication')
 const { authoriseAdmin, authoriseCommittee, authoriseUser } = require('../../middleware/authorisation')
 const { createUser, getUsers, getUserById, updateUser, deleteUser } = require('./userController');
@@ -12,12 +12,12 @@ router.get('/users', authenticateUser, getUsers);
 router.get('/users/:id', authenticateUser, getUserById);
 
 // Create a new user
-router.post('/users', validateUser, createUser);
+router.post('/users', validateNewUser, createUser);
 
 // Update a user by ID
-router.put('/users/:id', authenticateUser, validateUser, updateUser);
+router.put('/users/:id', authenticateUser, validateUpdateUser, updateUser);
 
 // Delete a user by ID
-router.delete('/users/:id', authenticateUser, validateUser, deleteUser);
+router.delete('/users/:id', authenticateUser, validateDeleteUser, deleteUser);
 
 module.exports = router;
--- src/middleware/userValidation.js
+++ src/middleware/userValidation.js
@@ -1,8 +1,8 @@
-const { validationResult, body } = require('express-validator');
+const { validationResult, body, check, param } = require('express-validator');
 const User = require('../models/User');
 
 // Validate user input
-const validateUser = [
+const validateNewUser = [
     body('username')
         .notEmpty()
         .withMessage('Email is required')
@@ -46,4 +46,61 @@ const validateUser = [
     }
 ];
 
-module.exports = { validateUser }
+// Validate user input
+const validateUpdateUser = [
+    param('id')
+        .isMongoId()
+        .withMessage('Invalid user ID'),
+    check('*')
+        .notEmpty()
+        .withMessage('Must update at least one field'),
+    body('username')
+        .optional()
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Invalid email format')
+        .custom(async (value) => {
+            const user = await User.findOne({ email: value });
+            if (user) {
+                throw new Error('Email is already registered');
+            }
+            return true;
+        }),
+    body('password')
+        .optional()
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    body('name')
+        .optional()
+        .trim()
+        .notEmpty()
+        .withMessage('Name is required')
+        .matches(/^[^0-9]+$/)
+        .withMessage('Name must not contain numbers'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array().map((error) => error.msg) });
+        }
+        next();
+    }
+];
+
+// Validate user input
+const validateDeleteUser = [
+    param('id')
+        .isMongoId()
+        .withMessage('Invalid user ID'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(404).json({ errors: errors.array().map((error) => error.msg) });
+        }
+        next();
+    }
+];
+
+module.exports = { validateNewUser, validateUpdateUser, validateDeleteUser }
