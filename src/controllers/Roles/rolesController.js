e4c82fb0f084f2df346be184d8431be504340961
socialtonics <mjkgarrow@gmail.com>
2023-07-10 17:25:42 +1000

    code formatting

--- src/controllers/Roles/rolesController.js
+++ src/controllers/Roles/rolesController.js
@@ -2,10 +2,15 @@ const Role = require('../../models/Role')
 
 const getRoles = async (req, res) => {
     try {
+
         const roles = await Role.find();
+
         res.status(200).json(roles);
+
     } catch (error) {
+
         res.status(500).json({ error: error.message });
+
     }
 };
 
