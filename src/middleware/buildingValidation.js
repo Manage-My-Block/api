669aaa825c66357eb40c03ea5afe9080d3603840
socialtonics <mjkgarrow@gmail.com>
2023-07-12 23:03:40 +1000

    added building update validation

--- src/middleware/buildingValidation.js
+++ src/middleware/buildingValidation.js
@@ -86,5 +86,53 @@ const validateNewBuilding = [
     }
 ];
 
+// Validate user input
+const validateUpdatedBuilding = [
+    param('id')
+        .isMongoId()
+        .withMessage('Invalid user ID'),
+    check('*')
+        .notEmpty()
+        .withMessage('Must update at least one field'),
+    body('name')
+        .optional()
+        .trim()
+        .notEmpty()
+        .withMessage('Building name is required'),
+    body('apartmentCount')
+        .optional()
+        .isNumeric()
+        .withMessage('Apartment count must be a number'),
+    body('address')
+        .optional()
+        .trim()
+        .notEmpty()
+        .withMessage('Building address is required')
+        .custom(async (value) => {
+            // Search for user based on email field
+            const building = await Building.findOne({ address: value });
 
-module.exports = { validateNewBuilding }
+            // If user exists then email is already is use
+            if (building) {
+                throw new Error('Building address is already registered');
+            }
+
+            return true;
+        }),
+    (req, res, next) => {
+
+        // Check for errors
+        const errors = validationResult(req);
+
+        // If errors return an error response
+        if (!errors.isEmpty()) {
+            console.log(errors)
+            return res.status(400).json({ errors: errors.array().map((error) => error.msg) });
+        }
+        // Call next middleware
+        next();
+    }
+];
+
+
+module.exports = { validateNewBuilding, validateUpdatedBuilding }
