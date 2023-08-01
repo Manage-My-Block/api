ba3f6fcbb5026f4d3622540ef6f09da57299a472
socialtonics <mjkgarrow@gmail.com>
2023-08-01 12:26:47 +1000

    validating phone number and adding formatting to make it XX-XXXX-XXXX

--- src/middleware/contactValidation.js
+++ src/middleware/contactValidation.js
@@ -1,5 +1,25 @@
 const { validationResult, body, check, param } = require('express-validator');
 
+function padStringWithRandomNumbers(str) {
+    if (str.length >= 8) {
+        return str; // If the string is already 8 characters or more, return it as is
+    }
+
+    // Generate a random number between 0 and 999999 (6 digits)
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 1000000);
+    }
+
+    while (str.length < 8) {
+        const randomNumber = generateRandomNumber().toString();
+        str += randomNumber;
+    }
+
+    // Trim the string to a maximum length of 10 characters
+    return "03" + str.slice(0, 8);
+}
+
+
 // Validate contact creation data
 const validateNewContact = [
     // Validate email
@@ -11,7 +31,20 @@ const validateNewContact = [
     body('phoneNumber')
         .trim()
         .notEmpty()
-        .withMessage('Phone number is required'),
+        .withMessage('Phone number is required')
+        .customSanitizer((value) => {
+
+            // If value is length 8, add a fake location code (03 for Vic) to the start
+            if (value.length === 8) {
+                value = "03" + value
+            } else if (value.length < 8) { // If less than 8 then randomise some numbers to fill it out
+                value = padStringWithRandomNumbers(value)
+            }
+
+            // Make the string formatted like XX-XXXX-XXXX
+            return value.slice(0, 2) + "-" + value.slice(2, 6) + "-" + value.slice(6);
+
+        }),
     // Validate name
     body('name')
         .trim()
@@ -52,7 +85,19 @@ const validateUpdateContact = [
         .optional()
         .trim()
         .notEmpty()
-        .withMessage('Phone number is required'),
+        .withMessage('Phone number is required')
+        .isLength({ min: 8, max: 10 })
+        .withMessage('Phone number must be 8 - 10 numbers long')
+        .customSanitizer((value) => {
+
+            if (value.length <= 8) {
+                value = "03" + value
+            }
+
+            let cleanedNum = value.replace(/\D[^\.]/g, "");
+            return cleanedNum.slice(0, 3) + "-" + cleanedNum.slice(3, 6) + "-" + cleanedNum.slice(6);
+
+        }),
     body('occupation')
         .optional()
         .trim()
